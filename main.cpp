cc53eaaab4e6ff47a32e589b0006bae2291590b6
nikitka <iwannabe1337@icloud.com>
2022-03-27 18:02:37 +0300

    1.0 the main one

--- main.cpp
+++ main.cpp
@@ -1,5 +1,47 @@
 #include <iostream>
+#include <vector>
+#include <string>
+#include <sstream>
+
+using namespace std;
 
 int main() {
+
+	// локализаци€
+	setlocale(LC_ALL, "");
+
+	// переменные
+	int ks, reg;
+
+	reg = 3;
+
+	vector<string> sums;
+	vector<int> main;
+	string text, buffer;
+
+	// сколько сумматоров
+	cout << "¬ведите количество сумматоров: ";
+	cin >> ks;
+
+	// заполнение регистров нул€ми
+	for (int i = 0; i < reg; i++) {
+		main.push_back(0);
+	}
+
+	// заполнение регистров дл€ сумматоров
+	cout << "\n¬ведите регистры сумматоров: " << endl;
+	for (int i = 0; i < ks + 1; i++) {
+		getline(cin, buffer);
+		if (buffer.size() > 0) {
+			sums.push_back(buffer);
+		}
+	}
+
+	// текст дл€ кодировани€
+	cout << "\n¬ведите текст: ";
+	cin >> text;
+
+
+
 
 }
